f9dc9e95dd07205e1e12469c64593247f3ca5864
Tony Drake <t27duck@gmail.com>
2016-03-12 16:12:20 -0500

    Clean up weather plugin

--- plugins/weather.rb
+++ plugins/weather.rb
@@ -10,18 +10,14 @@ class Weather
     @cached_responses ||= {}
   end
 
-  match /weather (\w+)/i, :strip_colors => true
+  match /weather (\w+)/i, strip_colors: true
 
   def execute(m, search_term)
     return unless CONFIG["weather"]
 
     @term = search_term.downcase
-
     response = lookup_cache
-
-    if response.nil?
-      response = store_cache(make_request)
-    end
+    response = store_cache(make_request) if response.nil?
 
     m.reply response
   rescue
@@ -29,16 +25,14 @@ class Weather
 
   private ######################################################################
 
-  def handle_body(body)
-  end
-
   def make_request
-    url = URI.parse("http://api.openweathermap.org/data/2.5/weather?units=imperial&APPID=#{CONFIG["weather"]["api_key"]}&q=#{CGI.escape(@term)}")
+    search_key = @term.size == 5 ? 'zip' : 'q'
+    url = URI.parse("http://api.openweathermap.org/data/2.5/weather?units=imperial&APPID=#{CONFIG["weather"]["api_key"]}&#{search_key}=#{CGI.escape(@term)}")
     req = Net::HTTP::Get.new(url.to_s)
     res = Net::HTTP.start(url.host, url.port) {|http|
-        http.request(req)
+      http.request(req)
     }
-    return unless res.code == "200"
+    return unless res.code == '200'
     JSON.parse(res.body)
   end
 
@@ -52,8 +46,8 @@ class Weather
 
   def store_cache(body)
     @cached_responses[@term] = {
-      :body => "Currently in #{body["name"]} - #{body["weather"].first["description"]}, #{body["main"]["temp"]}F, Humidity #{body["main"]["humidity"]}%. Powered by openweathermap.org",
-      :last_request => Time.now
+      body: "Currently in #{body['name']} - #{body['weather'].first['description']}, #{body['main']['temp']}F, Humidity #{body['main']['humidity']}%. Powered by openweathermap.org",
+      last_request: Time.now
     }
     @cached_responses[@term][:body]
   end

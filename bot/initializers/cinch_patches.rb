3099148a8a121f92094e00fc8e96f5dab9ad6b5e
Tony Drake <t27duck@gmail.com>
2016-04-05 20:59:20 -0400

    Delay logging self to let other logging threads run first (in theory)

--- bot/initializers/cinch_patches.rb
+++ bot/initializers/cinch_patches.rb
@@ -1,6 +1,13 @@
 module LogReply
   def reply(text, prefix = false)
+    super
     if @channel
+      # Wait one second so the thread that is recording
+      # the orignal input gets a chance to write to the
+      # database. Not the best solution but should work
+      # for most cases... I'm a monster.
+      sleep 1
+
       RawChatLog.create!(
         channel:    @channel,
         nick:       bot.nick,
@@ -10,13 +17,7 @@ module LogReply
         created_on: Time.now.utc
       )
     end
-
-    super
   end
 end
 
-module Cinch
-  class Message
-    prepend LogReply
-  end
-end
+Cinch::Message.prepend LogReply

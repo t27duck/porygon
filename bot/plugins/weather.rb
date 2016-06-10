46d360a0b7a6c2830a4b41eb89cad72c37d145d4
Tony Drake <t27duck@gmail.com>
2016-06-10 19:19:21 -0400

    Remove response cache from weather plugin

--- bot/plugins/weather.rb
+++ bot/plugins/weather.rb
@@ -1,42 +1,25 @@
 class Weather
   include Cinch::Plugin
 
-  def initialize(*args)
-    super
-    @cached_responses ||= {}
-  end
-
   match(/weather ([\w, ]+)/i, strip_colors: true)
 
   def execute(m, search_term)
     return unless CONFIG["weather"]
 
-    @term = search_term.downcase
-    response = lookup_cache
-    response = store_cache(make_request) if response.nil?
-
-    m.reply response
+    term = search_term.downcase
+    m.reply make_request(term)
   rescue => e
     puts e.message
   end
 
   private ######################################################################
 
-  def make_request
-    terms = @term.split(',').map(&:strip).reverse
-    api_wrapper.conditions_for(*terms)
+  def make_request(term)
+    terms = term.split(',').map(&:strip).reverse
+    parse_response api_wrapper.conditions_for(*terms)
   end
 
-  def lookup_cache
-    cached_response = @cached_responses[@term]
-    # last_request is in seconds
-    if cached_response && Time.now - cached_response[:last_request] > 900
-      return cached_response[:body]
-    end
-    nil
-  end
-
-  def store_cache(body)
+  def parse_response(body)
     if body['response']['error'] && body['response']['error']['description']
       body = body['response']['error']['description']
     else
@@ -47,11 +30,7 @@ class Weather
       body << "Wind: #{ob['wind_string']}. "
       body << "Powered by Weather Underground."
     end
-    @cached_responses[@term] = {
-      body: body,
-      last_request: Time.now
-    }
-    @cached_responses[@term][:body]
+    body
   end
 
   def api_wrapper

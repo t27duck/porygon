b9872421cffcb9dee46d1953c8808181d747a5bc
Tony Drake <t27duck@gmail.com>
2018-01-20 21:26:56 -0500

    Added math plugin

---
+++ bot/plugins/math.rb
@@ -0,0 +1,12 @@
+class Math
+  include Cinch::Plugin
+
+  match(/\Amath ([\(\)\d\+\-*^% \/\.]+)\z/, strip_colors: true)
+
+  def execute(m, math)
+    return if math.size > 50
+    m.reply eval(math)
+  rescue => e
+    puts e.message
+  end
+end
